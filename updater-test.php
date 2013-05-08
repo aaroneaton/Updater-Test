b597c9a9f743f107dcb29e5b851a3fd8b1e23dbd
J. Aaron Eaton <aaron@channeleaton.com>
2013-05-08 15:44:02 -0500

    Upping the version number to test

--- updater-test.php
+++ updater-test.php
@@ -24,7 +24,7 @@ License:
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-  ~Version: 0.1~
+  ~Version: 0.2~
 
 */
 
